8ebb47e2ba7c56eda1610542ddcdd592e5154659
baiguai <baiguai.junk@gmail.com>
2026-02-13 01:10:44 -0800

    Initial build.

---
+++ build/CMakeFiles/mnemothoth.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mnemothoth_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/mnemothoth_autogen.dir/ParseCache.txt"
+  "mnemothoth_autogen"
+  "CMakeFiles/mnemothoth.dir/mnemothoth_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/mnemothoth.dir/mnemothoth_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/mnemothoth.dir/src/main.cpp.o"
+  "CMakeFiles/mnemothoth.dir/src/main.cpp.o.d"
+  "CMakeFiles/mnemothoth.dir/src/mainwindow.cpp.o"
+  "CMakeFiles/mnemothoth.dir/src/mainwindow.cpp.o.d"
+  "mnemothoth"
+  "mnemothoth.pdb"
+  "mnemothoth_autogen/mocs_compilation.cpp"
+  "mnemothoth_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mnemothoth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
